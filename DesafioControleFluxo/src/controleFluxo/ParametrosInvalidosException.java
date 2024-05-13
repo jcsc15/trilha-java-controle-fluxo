62117612169f629089a9f04f384cbeb872651fe7
jcsc15 <kucip2234@gmail.com>
2024-05-13 09:50:31 -0300

    First commit

---
+++ DesafioControleFluxo/src/controleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+package controleFluxo;
+
+public class ParametrosInvalidosException  extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public  ParametrosInvalidosException(String mensagem) {
+		super (mensagem);
+}
+}
